21b384c5101e8735763cd07edf3b9c98b5e57362
oknkc8 <oknkc8@gmail.com>
2021-05-11 16:42:11 +0900

    add preparing scannet script

--- prepare_data_scannet.sh
+++ prepare_data_scannet.sh
@@ -1,4 +1,4 @@
-python prepare_data.py --path DATAROOT_SCANNET --path_meta METAROOT_SCANNET --dataset scannet --i 0 --n 4 & \
-python prepare_data.py --path DATAROOT_SCANNET --path_meta METAROOT_SCANNET --dataset scannet --i 1 --n 4 & \
-python prepare_data.py --path DATAROOT_SCANNET --path_meta METAROOT_SCANNET --dataset scannet --i 2 --n 4 & \
-python prepare_data.py --path DATAROOT_SCANNET --path_meta METAROOT_SCANNET --dataset scannet --i 3 --n 4
+python prepare_data.py --path DATAROOT_SCANNET --path_meta METAROOT_SCANNET --dataset scannet --i 0 --n 4 --test & \
+python prepare_data.py --path DATAROOT_SCANNET --path_meta METAROOT_SCANNET --dataset scannet --i 1 --n 4 --test & \
+python prepare_data.py --path DATAROOT_SCANNET --path_meta METAROOT_SCANNET --dataset scannet --i 2 --n 4 --test & \
+python prepare_data.py --path DATAROOT_SCANNET --path_meta METAROOT_SCANNET --dataset scannet --i 3 --n 4 --test
